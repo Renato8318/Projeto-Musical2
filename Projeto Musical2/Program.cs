6352041c766e53a3f2102d92fa83b2b5aa3cb1dd
Renato Paiva <paivarenato08@gmail.com>
2023-10-10 12:46:16 -0300

    Centralizando acesso

--- Projeto Musical2/Program.cs
+++ Projeto Musical2/Program.cs
@@ -2,13 +2,14 @@
 musica1.nome = "Walk";
 musica1.artista = "Foo Fighters";
 musica1.duracao = 275;
-musica1.disponivel = true;
+musica1.EscreveDisponivel(true);
+Console.WriteLine(musica1.LeDisponivel);
 
 Musica musica2 = new Musica();
 musica2.nome = "Vertigo";
 musica2.artista = "U2";
 musica2.duracao = 375;
-musica2.disponivel= false;
+musica2.EscreveDisponivel(false);
 
 musica1.ExibirFichaTecnica();
 musica2.ExibirFichaTecnica();
