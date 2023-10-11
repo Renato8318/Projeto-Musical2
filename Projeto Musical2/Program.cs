91dd53e3718a86bd33788a5dd6fef1c86b879cb2
Renato Paiva <paivarenato08@gmail.com>
2023-10-11 14:18:45 -0300

    Criando construtor e relacionamento entre classes

--- Projeto Musical2/Program.cs
+++ Projeto Musical2/Program.cs
@@ -1,18 +1,21 @@
-﻿Album albumDoQueen = new Album();
+﻿Banda queen = new Banda();
+queen.Nome = "Queen";
+
+Album albumDoQueen = new Album();
 albumDoQueen.Nome = "A nigth at the opera";
 
-Musica musica = new Musica();
+Musica musica = new Musica(queen);
 musica.Nome = "Love of my life";
 musica.Duracao = 213;
 
-Musica musica2 = new Musica();
+Musica musica2 = new Musica(queen);
 musica2.Nome = "Bohemian Rhapsody";
 musica2.Duracao = 354;
 
 albumDoQueen.AdicionarMusica(musica);
 albumDoQueen.AdicionarMusica(musica2);
 
-Banda queen = new Banda();
-queen.Nome = "Queen";
+
+albumDoQueen.ExibirMusicasDoAlbum();
 queen.AdicionarAlbum(albumDoQueen);
 queen.ExibirDiscografia();
